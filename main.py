14df6ab43deb09ba3ae9cb54e121900b84607000
Dioneya <nikita_kunov8@mail.ru>
2020-10-14 16:32:58 +0300

    bot_demo 1.0

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import random, vk_api, vk
+from setting import main_token, group_id, message_key, server_, ts_, command_name
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
-vk_session = vk_api.VkApi(token='260b77719a995237e89ac3aa7b8bb2b07a19804602dffc7f739f6745eb11dd9986dc5e2139d30853ded8f')
+vk_session = vk_api.VkApi(token = main_token)
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-longpoll = VkBotLongPoll(vk_session, 193323157)
+longpoll = VkBotLongPoll(vk_session, group_id)
 vk = vk_session.get_api()
 from vk_api.longpoll import VkLongPoll, VkEventType
 Lslongpoll = VkLongPoll(vk_session)
 Lsvk = vk_session.get_api()
 
-command_name = 'Оксения' #имя на которое бот будет триггериться 
-
 #Является ли данное сообщение командой?
 def Is_Command(message_):
-	return command_name in str(message_)
+	return command_name in str(message_).lower()
 
 #В параметрах 1) Текст сообещния, 2) Ссылка на прикреплённый объект
 def Send_Message(message_, link_attachment = ''):
 	vk.messages.send(
-                    key = ('497dddd12ab57e45b2078e39124c70ec3a546118'),
-                    server = ('https://lp.vk.com/wh193323157'),
-                    ts=('11'),
+                    key = (message_key),
+                    server = (server_),
+                    ts=(ts_),
                     random_id = get_random_id(),
               	    message = message_,
               	    peer_id=event.object.peer_id,
@@ -28,22 +27,44 @@ def Send_Message(message_, link_attachment = ''):
             	    chat_id = event.chat_id
                     )
 
+def Check_Woman_Logic():
+	try:
+		Check_Woman_Logic.a += 1
+	except AttributeError:
+		Check_Woman_Logic.a = 0
+	if(Check_Woman_Logic.a==0):
+		Send_Message('Ты охуел?')
+	elif(Check_Woman_Logic.a==1):
+		Send_Message('Ну не хочу')
+	else:
+		Send_Message('Ладно...')
+		Check_Woman_Logic.a = 0
+
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
-
-        if Is_Command(event) and ('Ку' in str(event) or 'Привет' in str(event) or 'Хай' in str(event) or 'Хелло' in str(event) or 'Хеллоу' in str(event)):
-            if event.from_chat:
-                Send_Message('Привет!')
-
-        elif 'Ксюша' in str(event):
-        	if event.from_chat:
-        		Send_Message('Какая нахуй Ксюша?? Она блять Оксана!!!!')
-
-       	elif Is_Command(event) and ('дз' and 'скинули' in str(event)):
-       		if event.from_chat:
-       			Send_Message('','photo94890674_457245477')
+    	id_account = event.object['message']['from_id']
+    	account_info = vk.users.get(user_ids = id_account)
+    	print(account_info)
+    	print(account_info[0]['first_name'])
+    	if Is_Command(event):
+    		if ('ку' in str(event).lower() or 'привет' in str(event).lower() or 'хай' in str(event).lower() or 'хелло' in str(event).lower() or 'хеллоу' in str(event).lower()):
+    			if event.from_chat:
+    				Send_Message(account_info[0]['first_name']+', привет!')
+    		elif 'дз' and 'скинули' in str(event):
+    			if event.from_chat:
+    				Send_Message('','photo94890674_457245477')
+    		elif 'нюхай' and 'бебру' in str(event):
+    			if event.from_chat:
+    				Send_Message(account_info[0]['first_name']+', Ты охуел? Сам нюхай!!', 'video258466863_456239268')
+    		elif 'отсоси' in str(event) or 'соси' in str(event):
+    			Check_Woman_Logic()
+    		else:
+    			if event.from_chat:
+    				Send_Message('Неверная команда, иди нахуй')
+    	elif 'Ксюша' in str(event):
+    		if event.from_chat:
+    			Send_Message('Какая нахуй Ксюша?? Она блять Оксана!!!!')
+       	
        			
-       	elif Is_Command(event) and ('нюхай' and 'бебру' in str(event)):
-       		if event.from_chat:
-       			Send_Message('Ты охуел? Сам нюхай!!', 'video258466863_456239268')
+       	
        			
---
+++ setting.py
@@ -0,0 +1,9 @@
+main_token = '260b77719a995237e89ac3aa7b8bb2b07a19804602dffc7f739f6745eb11dd9986dc5e2139d30853ded8f'
+group_id = 193323157
+
+#данные для отправки сообщения
+message_key = '497dddd12ab57e45b2078e39124c70ec3a546118'
+server_ = 'https://lp.vk.com/wh193323157'
+ts_ = '11'
+
+command_name = 'оксения' #имя на которое бот будет триггериться 
